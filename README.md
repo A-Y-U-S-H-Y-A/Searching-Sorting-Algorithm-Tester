53dd72a21539a23c1d111a5fd094bb68cd63aac0
Ayushya Shah <ayushya.shah1201@gmail.com>
2022-05-10 12:22:28 +0530

    Code improvement

---
+++ README.md
@@ -0,0 +1,30 @@
+# Searching-Sorting-Algorithm-Tester
+
+Searching-Sorting-Algorithm-Tester is a Python Program using the power of permutations and combinations to find the best, average and worst case of one or more searching/sorting algorithm(s) with a given dataset
+
+## Requirements
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install the requirements
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+Go to the newly downloaded directory and run
+
+### For Sorting
+```bash
+python sorting.py
+```
+
+### For Searching
+```bash
+python searching.py
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- sorting.py
+++ sorting.py
@@ -3,14 +3,20 @@ from datetime import datetime
 from tabulate import tabulate
 import random
 
-def bubble(arr):
+# IMPORTANT VARIABLES
+ALGORITHM_NAMES = ['Bubble Sort', 'Selection Sort', 'Merge Sort', 'Quick Sort', 'Insertion Sort']
+ALGORITHM_FUNCTION_NAMES = ['bubble', 'selection', 'merge', 'quick', 'insertion'] #TO BE IN SAME ORDER AS NAMES
+ALGORITHM_COUNT = len(ALGORITHM_NAMES)
+
+
+def bubble(start,end,arr):
     n = len(arr)
     for i in range(n):
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1] :
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
-def selection(arr):
+def selection(start,end,arr):
     for i in range(len(arr)):
         min_idx = i
         for j in range(i+1, len(arr)):
@@ -18,13 +24,13 @@ def selection(arr):
                 min_idx = j
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
 
-def merge(arr):
+def merge(start,end,arr):
     if len(arr) > 1:
         mid = len(arr)//2
         L = arr[:mid]
         R = arr[mid:]
-        merge(L)
-        merge(R)
+        merge(0,0,L)
+        merge(0,0,R)
         i = j = k = 0
         while i < len(L) and j < len(R):
             if L[i] < R[j]:
@@ -63,7 +69,7 @@ def quick(start, end, arr):
         quick(p + 1, end, arr)
 
 
-def insertion(arr):
+def insertion(start, end, arr):
     for i in range(1, len(arr)):
         key = arr[i]
         j = i-1
@@ -73,227 +79,168 @@ def insertion(arr):
         arr[j + 1] = key
 
 
+# CHOOSING ALGORITHMS
+def algorithms():
+    print('Enter 1 for comparing one or more searching types')
+    print('Enter 2 for analysis of a single algorithm')
+    som = int(input('Enter choice:'))
+    algs=[]
+    if(som == 1):
+        while True:
+            print('Enter Algorithm Number')
+            name_iter = 1
+            for name in ALGORITHM_NAMES:
+                print(str(name_iter) + ': '+name)
+                name_iter +=1
+            print('0: Continue')
+            alg = int(input('Enter choice:'))
+            if alg == 0:
+                break
+            if len(algs) == ALGORITHM_COUNT:
+                break
+            if (alg not in algs) and alg >= 1 and alg <= ALGORITHM_COUNT:
+                algs.append(alg)
+        return [som,algs]
 
-
-
-print('Enter 1 for comparing one or more sorting types')
-print('Enter 2 for analysis of a single algorithm')
-som = int(input('Enter choice:'))
-algs=[]
-if(som == 1):
-    while True:
+    if (som == 2):
         print('Enter Algorithm Number')
-        print('1: Bubble Sort')
-        print('2: Selection Sort')
-        print('3: Merge Sort')
-        print('4: Quick Sort')
-        print('5: Insertion Sort')
-        print('0: Continue')
-        alg = int(input('Enter choice:'))
-        if alg == 0:
-            break
-        if len(algs) == 5:
-            break
-        if alg not in algs:
-            algs.append(alg)
-    
+        name_iter = 1
+        for name in ALGORITHM_NAMES:
+            print(str(name_iter) + ': '+name)
+            name_iter +=1
+        while True:
+            alg = int(input('Enter choice:'))
+            if alg >= 1 and alg <= ALGORITHM_COUNT:
+                break
+        return [som,alg]
 
-if (som == 2):
-    print('Enter Algorithm Number')
-    print('1: Bubble Sort')
-    print('2: Selection Sort')
-    print('3: Merge Sort')
-    print('4: Quick Sort')
-    print('5: Insertion Sort')
-    alg = int(input('Enter choice:'))
 
-print('Enter 1 for a custom dataset')
-print('Enter 2 for a random dataset')
-dc=int(input('Enter choice:'))
-if(dc == 1):
-    try:
-        data = input('Enter Dataset e.g. [1,2,3,4,5,6]: ')    
-        if(data.index('[')!=0 and data.rindex(']')!= (len(data)-1)):
-            raise ValueError
-        for x in range(1,len(data)-1):
-            if ord(data[x]) == 32 or (ord(data[x])>=48 and ord(data[x])<=57) or ord(data[x]) == 44 :
-                continue
-            else:
-                raise ValueError
-        num = ''
-        fl = []
-        for z in range(1,len(data)-1):
-            if ord(data[z]) == 44:
-                if len(num)>=1:
+# DEFINING DATASET
+def dataset():
+    print('Enter 1 for a custom dataset')
+    print('Enter 2 for a random dataset')
+    dc=int(input('Enter choice:'))
+    if(dc == 1):
+        while True:
+            try:
+                data = input('Enter Dataset e.g. [1,2,3,4,5,6]: ')    
+                if(data.index('[')!=0 and data.rindex(']')!= (len(data)-1)):
+                    raise ValueError
+                for x in range(1,len(data)-1):
+                    if ord(data[x]) == 32 or (ord(data[x])>=48 and ord(data[x])<=57) or ord(data[x]) == 44 :
+                        continue
+                    else:
+                        raise ValueError
+                num = ''
+                fl = []
+                for z in range(1,len(data)-1):
+                    if ord(data[z]) == 44:
+                        if len(num)>=1:
+                            fl.append(int(num))
+                            num =''
+                    if (ord(data[z])>=48 and ord(data[z])<=57):
+                        num+= data[z]
+                if(len(num)>=1):
                     fl.append(int(num))
-                    num =''
-            if (ord(data[z])>=48 and ord(data[z])<=57):
-                num+= data[z]
-        if(len(num)>=1):
-            fl.append(int(num))
+                if(len(fl)<2):
+                    print('List must have atleast 2 values')
+                    raise ValueError
+                break
 
-    except Exception as e:
-        print(e)
-    except:
-        print('Invalid List')
-else:
-    try:
-        dl = int(input('Enter dataset length:'))
-        if(dl<1):
-            raise ValueError
-    except:
-        print('Length must be a positive integer greater than 0. Set to default (5)')
-        dl = 5
-    fl = []
-    for k in range(dl):
-        fl.append(random.randint(1, 10000))
+            except Exception as e:
+                print(e)
+            except:
+                print('Invalid List')
+    else:
+        try:
+            dl = int(input('Enter dataset length:'))
+            if(dl<2):
+                raise ValueError
+        except:
+            print('Length must be a positive integer greater than 2. Set to default (5)')
+            dl = 5
+        fl = []
+        for k in range(dl):
+            fl.append(random.randint(1, 10000))
 
-data = fl
+    data = fl
+    return data
 
 
-perm = permutations(data)
-tn = 0
-if(som == 1):
-    info = {} #"Bubble":[max,min,avg]
-    for i in list(perm):
-        tn+=1
-        if(1 in algs):
-            tl = list(i)
-            temp = datetime.now()
-            bubble(tl)
-            temp2 = datetime.now()
-            tt = ((temp2 - temp).total_seconds())
-            if('Bubble' in info):
-                info['Bubble'][2] += tt
-                if(info['Bubble'][0] < tt):
-                    info['Bubble'][0] = tt
-                if(info['Bubble'][1] > tt and tt > 0):
-                    info['Bubble'][1] = tt
-            else:
-                info['Bubble'] = [0,0,0]
-                info['Bubble'][0] = tt
-                info['Bubble'][1] = 9999
-                info['Bubble'][2] += tt
-        if(2 in algs):
-            tl = list(i)
-            temp = datetime.now()
-            selection(tl)
-            temp2 = datetime.now()
-            tt = ((temp2 - temp).total_seconds())
-            if('Selection' in info):
-                info['Selection'][2] += tt
-                if(info['Selection'][0] < tt):
-                    info['Selection'][0] = tt
-                if(info['Selection'][1] > tt and tt > 0 ):
-                    info['Selection'][1] = tt
-            else:
-                info['Selection'] = [0,0,0]
-                info['Selection'][0] = tt
-                info['Selection'][1] = 9999
-                info['Selection'][2] += tt
-        if(3 in algs):
-            tl = list(i)
-            temp = datetime.now()
-            merge(tl)
-            temp2 = datetime.now()
-            tt = ((temp2 - temp).total_seconds())
-            if('Merge' in info):
-                info['Merge'][2] += tt
-                if(info['Merge'][0] < tt):
-                    info['Merge'][0] = tt
-                if(info['Merge'][1] > tt and tt > 0):
-                    info['Merge'][1] = tt
-            else:
-                info['Merge'] = [0,0,0]
-                info['Merge'][0] = tt
-                info['Merge'][1] = 9999
-                info['Merge'][2] += tt
-        if(4 in algs):
-            tl = list(i)
-            temp = datetime.now()
-            quick(0,len(tl)-1,tl)
-            temp2 = datetime.now()
-            tt = ((temp2 - temp).total_seconds())
-            if('Quick' in info):
-                info['Quick'][2] += tt
-                if(info['Quick'][0] < tt):
-                    info['Quick'][0] = tt
-                if(info['Quick'][1] > tt and tt > 0):
-                    info['Quick'][1] = tt
-            else:
-                info['Quick'] = [0,0,0]
-                info['Quick'][0] = tt
-                info['Quick'][1] = 9999
-                info['Quick'][2] += tt
-        if(5 in  algs):
-            tl = list(i)
-            temp = datetime.now()
-            insertion(tl)
-            temp2 = datetime.now()
-            tt = ((temp2 - temp).total_seconds())
-            if('Insertion' in info):
-                info['Insertion'][2] += tt
-                if(info['Insertion'][0] < tt):
-                    info['Insertion'][0] = tt
-                if(info['Insertion'][1] > tt and tt > 0):
-                    info['Insertion'][1] = tt
-            else:
-                info['Insertion'] = [0,0,0]
-                info['Insertion'][0] = tt
-                info['Insertion'][1] = 9999
-                info['Insertion'][2] += tt
-#Beautification
-    mt = [['Algorithm Name','Minimum Time','Maximum Time','Average Time']]
-    for title in info:
-        ea = []
-        ea.append(title + ' Sort')
-        ea.append((str("{:.9f}".format(info[title][1])) + ' s'))
-        ea.append((str("{:.9f}".format(info[title][0])) + ' s'))
-        ea.append((str("{:.9f}".format(info[title][2]/(tn*1.0))) + ' s'))
-        mt.append(ea)
-    print()
-    print(tabulate(mt, headers='firstrow', tablefmt='fancy_grid'))
-    print()
-    
+# RUNNING THE PROCESSING
+def run_test(som,algs,search,perm):
+    tn = 0
+    if(som == 1): # Multiple algos
+        info = {} # "Linear":[max,min,avg]
+        for i in list(perm): # For every permutation
+            tn+=1
+            for algoindex in algs: # For every algorithm selected
+                ai = int(algoindex) - 1
+                temp = datetime.now()
+                globals()[ALGORITHM_FUNCTION_NAMES[ai]](0,len(list(i)),list(i)) # Call algorithm
+                temp2 = datetime.now()
+                tt = ((temp2 - temp).total_seconds()) # Time taken
+                name_of_algo = ALGORITHM_NAMES[ai]
+                if(name_of_algo in info): # Record exists
+                    info[name_of_algo][2] += tt
+                    if(info[name_of_algo][0] < tt):
+                        info[name_of_algo][0] = tt
+                    if(info[name_of_algo][1] > tt and tt > 0):
+                        info[name_of_algo][1] = tt
+                else:
+                    info[name_of_algo] = [0,0,0]
+                    info[name_of_algo][0] = tt
+                    info[name_of_algo][1] = 9999
+                    info[name_of_algo][2] += tt
+        
+    # Beautification
 
-if(som == 2):
-    avg = 0.0
-    mini = 9999
-    maxi = 9999
-    for i in list(perm):
-        tn+=1
-        i = list(i)
-        if(alg == 1):
-           temp = datetime.now()
-           bubble(i)
-           temp2 = datetime.now()
-           avg += ((temp2 - temp).total_seconds())
-        elif(alg == 2):
+        mt = [['Algorithm Name','Minimum Time','Maximum Time','Average Time']]
+        for title in info:
+            ea = []
+            ea.append(title + ' Search')
+            ea.append((str("{:.9f}".format(info[title][1])) + ' s'))
+            ea.append((str("{:.9f}".format(info[title][0])) + ' s'))
+            ea.append((str("{:.9f}".format(info[title][2]/float(tn))) + ' s'))
+            mt.append(ea)
+        print()
+        print(tabulate(mt, headers='firstrow', tablefmt='fancy_grid'))
+        print()
+        
+
+    if(som == 2):
+        alg = algs - 1
+        avg = 0.0
+        mini = 2e400
+        maxi = 0.0
+        for i in list(perm):
             temp = datetime.now()
-            selection(i)
+            globals()[ALGORITHM_FUNCTION_NAMES[alg]](0,len(list(i)),list(i)) # Call algorithm
             temp2 = datetime.now()
-            avg += ((temp2 - temp).total_seconds())
-        elif(alg == 3):
-            temp = datetime.now()
-            merge(i)
-            temp2 = datetime.now()
-            avg += ((temp2 - temp).total_seconds())
-        elif(alg == 4):
-            temp = datetime.now()
-            quick(0, len(i)-1,i)
-            temp2 = datetime.now()
-            avg += ((temp2 - temp).total_seconds())
-        else:
-            temp = datetime.now()
-            insertion(i)
-            temp2 = datetime.now()
-            avg += ((temp2 - temp).total_seconds())
-        if(maxi == 9999 and ((temp2 - temp).total_seconds()) > 0):
-            maxi = ((temp2 - temp).total_seconds())
-            mini = ((temp2 - temp).total_seconds())
-        if(maxi < ((temp2 - temp).total_seconds())):
-            maxi = ((temp2 - temp).total_seconds())
-        if(mini > ((temp2 - temp).total_seconds()) and ((temp2 - temp).total_seconds()) > 0):
-            mini = ((temp2 - temp).total_seconds())
-    print('Average time :',("{:.9f}".format((avg/(tn*1.0)))),' seconds')
-    print('Time Range: ',("{:.9f}".format(mini)),'s - ',("{:.9f}".format(maxi)),'s')
+            tt = ((temp2 - temp).total_seconds()) # Time taken
+            if (tn == 0):
+                if tt > 0:
+                    mini = tt
+                maxi = tt
+                avg = tt
+            else:
+                avg += tt
+                if tt > 0:
+                    mini = min(tt,mini)
+                maxi = max(tt,maxi)
+            tn += 1
+        avg = avg / float(tn)
+        print('Average time :',("{:.9f}".format(avg)),' seconds')
+        print('Time Range: ',("{:.9f}".format(mini)),'s - ',("{:.9f}".format(maxi)),'s')
+
+
+def main():
+    number_of_algos, algos = algorithms()
+    data = dataset()
+    search = data[0]
+    perm = permutations(data)
+    run_test(number_of_algos, algos, search, perm)
+
+
+if __name__ == "__main__":
+    main()
